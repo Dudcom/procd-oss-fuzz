bae27fb6440116f59b1a44d572cbfb871e5b0fa2
Dudcom <70006792+Dudcom@users.noreply.github.com>
2025-06-25 18:42:29 -0400

    pushing mywork

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ export CFLAGS="$CFLAGS -I$INSTALL_DIR/include -D_GNU_SOURCE -std=gnu99"
 export LDFLAGS="$LDFLAGS -L$INSTALL_DIR/lib"
 
 # Generate capabilities-names.h (required by jail/capabilities.c)
-./make_capabilities_h.sh "$CC" > capabilities-names.h
+bash ./make_capabilities_h.sh "$CC" > capabilities-names.h
 
 # === Compile procd sources required for parseOCI ===
 # Build all jail/* and utils/utils.c as position-independent objects
